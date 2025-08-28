673b86c4a5c0a18f88bbeabdeac669d6c81f172c
andrewwy <andrewsit.wy@gmail.com>
2025-08-28 01:59:19 -0400

    Add pipes scrolling and marker for spawning

---
+++ Scenes/Pipes/Pipes.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+using System.Runtime.Serialization;
+
+public partial class Pipes : Node2D
+{
+	const float SCROLL_SPEED = 120.0f;
+
+	[Export] private VisibleOnScreenNotifier2D _visibleNotifier;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_visibleNotifier.ScreenExited += OnScreenExited;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		Position += new Vector2(-SCROLL_SPEED * (float)delta, 0);
+	}
+
+	public void OnScreenExited()
+	{
+		QueueFree();
+	}
+}
