21c2182adeac1ddd408338e8034c6b3e0597be00
andrewwy <andrewsit.wy@gmail.com>
2025-10-01 23:21:14 -0400

    Add high score label to Main scene and update its binding

--- Scenes/Main/Main.cs
+++ Scenes/Main/Main.cs
@@ -3,9 +3,12 @@ using Godot;
 public partial class Main : Control
 {
 
+	[Export] private Label _highScoreLabel;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_highScoreLabel.Text = $"{ScoreManager.GetHighScore():0000}";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
